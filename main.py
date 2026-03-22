8bd6ae7307edd062d5f6b437cab854c95995f17b
Ankit45264 <ankitkumarmaurya91881@gmail.com>
2026-03-22 14:04:12 +0530

    fix error

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import streamlit as st
 model = "Package_prediction_model.pkl"
 with open(model, "rb") as f:
     data = pickle.load(f)
-    print(data)
+    
 
 image=st.image("https://presidencyuniversity.in/uploads/cms_images/6829baa7694631747565223.webp",use_container_width=True)
 
